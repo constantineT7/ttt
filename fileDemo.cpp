2a15b2fb929ab744482a9cf4dba8a1c6bb16533b
twb <13373618344@qq.com>
2024-11-10 22:08:18 +0800

    code

---
+++ fileDemo.cpp
@@ -0,0 +1,157 @@
+﻿//demo 1   文件输入：文件操作的读取
+#define _CRT_SECURE_NO_WARNINGS   ///使用类scanf printf这样的函数需要在第一行包含该语句
+#define BUFLEN 256
+#include <stdio.h>
+#include <stdlib.h>
+#include<vector>
+#include <iostream>
+#include <set>
+#include<string>
+using namespace std;
+int main() {
+    vector<string>MYFile;
+    char lineBuf[BUFLEN];
+    FILE* source;
+    const char* pgm = "case2.txt";     ///test.txt文件需要和该代码源文件在同一个目录，注意查看是否隐藏了文件类型，避免文件为test.txt.txt
+    source = fopen(pgm, "r");    ////获得文件指针，"r"表示只读方式打开
+    while (1) {
+        if (fgets(lineBuf, BUFLEN - 1, source)) {  ///逐行读入文件内容，每次读入的内容都存放在lineBuf中
+            string tmep;
+            tmep = lineBuf;
+            MYFile.insert(MYFile.end(),tmep);
+        }
+        else {   ////已经到了文件末尾，可以直接退出
+            //end of file
+            break;
+        }
+    }  
+    fclose(source);   ///用完后要关闭文件指针
+    while (!MYFile.empty())
+    {
+        vector<string>  myFile;
+        for (int i = 0; i < 4; i++)
+        {
+            myFile.insert(myFile.end(), MYFile[0]);
+            cout << myFile[i];
+            MYFile.erase(MYFile.begin());
+        }
+        if(!MYFile.empty())MYFile.erase(MYFile.begin());
+
+        //变量赋值
+        vector<string> P1;
+        vector<string> P2;
+        set<char> Vp;
+        set<char> Vn;
+        char S;
+        //对Vp进行处理；
+        string s = myFile[0];
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ',')
+                continue;
+            Vp.insert(s[i]);
+        }
+        //对Vn进行处理
+        s = myFile[1];
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ',')
+                continue;
+            Vn.insert(s[i]);
+        }
+        //对P进行处理
+        s = myFile[2];
+        int start = 0;
+        int pos;
+        bool has1;
+        //cout << pos << endl;
+        int index_of_arrow;
+        string term;
+        string Arrow = "->";
+        string alpha, beta;
+        string beta1;
+        //对term进行分割
+        while (s.length() != 0)
+        {
+            pos = s.find(',');
+            term = s.substr(start, pos);
+            if (term.find(Arrow, 0) != string::npos)
+            {
+                has1 = false;
+                index_of_arrow = term.find(Arrow, 0);
+                alpha = term.substr(0, index_of_arrow);
+                beta = term.substr(index_of_arrow + 2, term.length() - index_of_arrow - 2);
+                int pos1 = beta.find('|');
+                while (pos1 > 0)
+                {
+                    has1 = true;
+                    beta1 = beta.substr(0, pos1);
+                    beta = beta.substr(pos1 + 1, beta.length() - pos);
+                    //cout << beta1 <<endl << beta<<endl;
+                    //cout <<"----------------"<<endl;
+                    pos1 = beta.find('|');
+                    P1.insert(P1.end(), alpha);
+                    P2.insert(P2.end(), beta1);
+                }
+                P1.insert(P1.end(), alpha);
+                P2.insert(P2.end(), beta);
+            }
+            if (pos < 0)
+                break;
+            s = s.substr(pos + 1, s.length() - pos);
+            if (has1) {
+                P1.insert(P1.end(), alpha);
+                P2.insert(P2.end(), beta1);
+                continue;
+            }
+        }
+        //判断文法
+        int one_of_type = 4;
+        int type = 4;//文法类型
+        for (int it = 0; it < P1.size(); it++)
+        {
+            if (one_of_type < type)
+                type = one_of_type;
+            alpha = P1[it];
+            beta = P2[it];
+            //判断0型文法
+            bool has_Vp = false;//旗帜
+            for (int i = 0; i < alpha.length(); i++) {
+                if (Vp.find(alpha[i]) != Vp.end()) {
+                    has_Vp = true;
+                    break;
+                }
+            }
+            one_of_type = 0;
+            if (!has_Vp) {
+                type = -1;
+                break;
+            }
+            //判断1型文法
+            if (alpha.length() <= beta.length() || beta.length() == 0)
+                one_of_type = 1;
+            else
+                continue;
+            //判断2型文法
+            if (alpha.length() == 1 && Vp.find(alpha[0]) != Vp.end())
+                one_of_type = 2;
+            else
+                continue;
+            //判断3型文法
+            if (beta.length() == 1 && Vn.find(beta[0]) != Vn.end())
+                one_of_type = 3;
+            else if (beta.length() == 2 && (Vn.find(beta[0]) != Vn.end() && Vp.find(beta[1]) != Vp.end()) || (Vn.find(beta[1]) != Vn.end() && Vp.find(beta[0]) != Vp.end()))
+                one_of_type = 3;
+            else
+                continue;
+        }
+        if (one_of_type < type)
+            type = one_of_type;
+        //最后输出文法
+        if (type == -1)
+            cout << "该文法不是正规文法" << endl;
+        else
+            cout << "该文法为 " << type << " 型文法" << endl;
+
+        cout << endl;
+
+    }
+}
---
+++ strtokDemo.cpp
@@ -0,0 +1,90 @@
+﻿///demo 2:通过分隔符来分隔字符串demo
+#define _CRT_SECURE_NO_WARNINGS   ///使用类scanf printf这样的函数需要在第一行包含该语句
+#include <stdlib.h>
+#include "stdio.h"
+#include "string.h"
+
+int strtokErr() {
+    char* temp = NULL;
+    char input[20] = "A=a,B=b,C=c,D=d";
+
+    printf("%s\n", input);
+    temp = strtok(input, ",");  ////strtok的第一个参数不能为指向常量的指针，我们使用字符数组就好
+    printf("temp:%s\n", temp);
+
+    while (temp != NULL) {     ////第一轮strtok循环
+        temp = strtok(NULL, ",");   ////分隔符为字符串时，则其中的每个字符都是分隔符.strtok里面有个静态变量会记录上次分割的地方，
+                                      //// 如果第一个参数为NULL，则会使用静态变量作为起始点,方便外面写循环。因此strtok函数需要避免嵌套调用
+        /// 调用会出错
+        if (temp != NULL) {
+            char* equal = temp;
+            equal = strtok(temp, "=");    ////在外层strtok的循环中不能调用strtok解析其他的字段，解决方法可以先把while中调用的结果先存起来，
+            printf("equal:%s\n", equal);     //////然后再调用strtok解析其他的字段。
+            while (equal != NULL) {
+                equal = strtok(NULL, "=");
+               printf("equal:%s\n", equal);
+            }
+        }
+        
+        printf("temp:%s\n", temp);
+    }
+    return 0;
+}
+
+
+int strtokOk() {
+    char* temp[5] = { NULL };
+    int i = 0;
+    char input[20] = "A=a,B=b,C=c,D=d";
+
+    printf("%s\n", input);
+    temp[0] = strtok(input, ",");  ////strtok的第一个参数不能为指向常量的指针，我们使用字符数组就好
+    printf("temp:%s\n", temp);
+
+    while (temp[i] != NULL) {
+        i++;
+        temp[i] = strtok(NULL, ",");   ////先存起来，避免的strtok的嵌套调用
+        printf("i:%d,temp:%s\n",i,temp[i]);
+    }
+
+    int j = 0;
+    while (j < i-1) {
+        if (temp[j] != NULL) {
+            char* left = temp[j];
+            left = strtok(temp[j], "=");
+            printf("left:%s\n", left);
+            while (left != NULL) {
+                left = strtok(NULL, "=");
+                printf("left:%s\n", left);
+            }
+        }
+        j++;
+    }
+    return 0;
+}
+
+int strtokSingle() {
+    char* temp = NULL;
+    char input[128] = "A=a,B=b,C=c,D=d";
+
+    printf("%s\n", input);
+    temp = strtok(input, ",");  ////strtok的第一个参数不能为指向常量的指针，我们使用字符数组就好
+    printf("temp:%s\n", temp);
+
+    while (temp != NULL) {
+        printf("len:%d\n", strlen(temp));    ///strlen可以获得已\0结尾的字符串的长度，需要包含头文件string.h
+        temp = strtok(NULL, ",");   ////strtok里面有个静态变量会记录上次分割的地方，
+        //// 如果第一个参数为NULL，则会使用静态变量作为起始点,方便外面写循环。因此strtok函数需要避免嵌套调用
+        printf("temp:%s\n", temp);
+    }
+    return 0;
+}
+
+
+//int main() {
+//    strtokSingle();
+
+ //   strtokErr();
+
+//    strtokOk();
+}
